27994c90ae7093719a798de8479ade341546c578
Xuanyao (Kelvin) Fong <xuanyao.fong@gmail.com>
2020-11-07 15:07:37 +0800

    Clean up script to launch MPI

--- spintronic/pbsscript.sh
+++ spintronic/pbsscript.sh
@@ -9,14 +9,14 @@
 export SIMDIR=~/proj/fpe/fantasi/spintronic
 export FNAME=large_field_0
 export SCRIPT=${FNAME}.py
-export MPICOMMAND="mpirun -np 8 "
-#export MPICOMMAND=""
+#export MPICOMMAND="mpirun -np 8 "
+export MPICOMMAND=""
 cd $TMPDIR
 rsync -az ${SIMDIR}/${SCRIPT} ./
 rsync -az ${SIMDIR}/HFields.py ./
 rsync -az ${SIMDIR}/meshes ./
 if [ -f ${SCRIPT} ]; then
-    module load singularity openmpi
+    module load singularity
     singularity run -B $TMPDIR /opt/containers/ubuntu/18/fenics_v1_10.sif ${MPICOMMAND} python ./${SCRIPT}
 fi
 if [ -d ${FNAME}_results ]; then
--- spintronic/pbsscript_cnnl.sh
+++ spintronic/pbsscript_cnnl.sh
@@ -3,21 +3,20 @@
 #PBS -l mpiprocs=8
 #PBS -l ncpus=8
 #PBS -l mem=16gb
-#PBS -l vmem=16gb
 #PBS -j oe
 
 export SIMDIR=~/proj/fpe/fantasi/spintronic
 export FNAME=large_field_0
 export SCRIPT=${FNAME}.py
-export MPICOMMAND="mpirun -np 8 "
+export MPICOMMAND="mpirun -np 8"
 #export MPICOMMAND=""
 cd $TMPDIR
 rsync -az ${SIMDIR}/${SCRIPT} ./
 rsync -az ${SIMDIR}/HFields.py ./
 rsync -az ${SIMDIR}/meshes ./
 if [ -f ${SCRIPT} ]; then
-    module load singularity cuda/10.0 gdrcopy libnl libibverbs openucx openmpi
-    singularity run -B $TMPDIR /opt/containers/ubuntu/18/fenics_v1_10.sif ${MPICOMMAND} python ./${SCRIPT}
+    module load singularity cuda/10.0 gdrcopy libnl libibverbs openucx openmpi pbspro
+    singularity run -B $TMPDIR -B ${PBSPROPATH}/spool /opt/containers/ubuntu/18/fenics_v1_10.sif ${MPICOMMAND} python ./${SCRIPT}
 fi
 if [ -d ${FNAME}_results ]; then
     echo "Directory containing final result files found! Copying back to home directory"
