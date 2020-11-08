8423ab19e06f4d7b5b2904db4420d64e4ef4c77e
Xuanyao Fong <xuanyao.fong@gmail.com>
2020-11-08 11:00:33 +0800

    Added relaxation_2.py

---
+++ spintronic/pbs_relax2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#PBS -l walltime=120:00:00
+#PBS -l mpiprocs=8
+#PBS -l ncpus=8
+#PBS -l mem=16gb
+#PBS -l vmem=16gb
+#PBS -j oe
+
+export SIMDIR=~/proj/fpe/fantasi/spintronic
+export FNAME=relaxation_2
+export SCRIPT=${FNAME}.py
+#export MPICOMMAND="mpirun -np 8 "
+export MPICOMMAND=""
+cd $TMPDIR
+rsync -az ${SIMDIR}/${SCRIPT} ./
+rsync -az ${SIMDIR}/rho_series.h5 ./
+rsync -az ${SIMDIR}/HFields.py ./
+rsync -az ${SIMDIR}/meshes ./
+if [ -f ${SCRIPT} ]; then
+    module load singularity
+    singularity run -B $TMPDIR /opt/containers/ubuntu/18/fenics_v1_10.sif ${MPICOMMAND} python ./${SCRIPT}
+fi
+if [ -d ${FNAME}_results ]; then
+    echo "Directory containing final result files found! Copying back to home directory"
+    tar czf ./${FNAME}.tgz ./${FNAME}_results
+    rsync -az ./${FNAME}.tgz ${SIMDIR}/.
+fi
