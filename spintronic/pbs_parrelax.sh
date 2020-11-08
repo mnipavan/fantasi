c841156139398cd399ba22430a4813a2e4ee59df
Xuanyao Fong <xuanyao.fong@gmail.com>
2020-11-08 14:55:00 +0800

    Test MPI using relaxation_3

--- spintronic/pbs_parrelax.sh
+++ spintronic/pbs_parrelax.sh
@@ -3,18 +3,21 @@
 #PBS -l mpiprocs=8
 #PBS -l ncpus=8
 #PBS -l mem=16gb
+#PBS -l vmem=16gb
 #PBS -j oe
 
 export SIMDIR=~/proj/fpe/fantasi/spintronic
-export FNAME=large_field_0
+export FNAME=relaxation_3
 export SCRIPT=${FNAME}.py
 export MPICOMMAND="mpirun -np 8"
+export OMP_NUM_THREADS=1
 cd $TMPDIR
 rsync -az ${SIMDIR}/${SCRIPT} ./
+rsync -az ${SIMDIR}/rho_series.h5 ./
 rsync -az ${SIMDIR}/HFields.py ./
 rsync -az ${SIMDIR}/meshes ./
 if [ -f ${SCRIPT} ]; then
-    module load singularity cuda/10.0 gdrcopy libnl libibverbs openucx openmpi pbspro
+    module load singularity openmpi pbspro
     singularity run -B $TMPDIR -B ${PBSPROPATH}/spool /opt/containers/ubuntu/18/fenics_v1_10.sif ${MPICOMMAND} python ./${SCRIPT}
 fi
 if [ -d ${FNAME}_results ]; then
